08139e67f9338622ea0979c1735b2fc991a3efdc
Lucas Campos Vieira <lucasscvvieira@gmail.com>
2022-10-01 11:58:29 -0300

    remove os arquivos gerados automaticamente

--- stacks/bare/k8s/setup_helm/tmgen_remote_state.tf
+++
@@ -1,9 +0,0 @@
-// TERRAMATE: GENERATED AUTOMATICALLY DO NOT EDIT
-// TERRAMATE: originated from generate_hcl block on /stacks/bare/k8s/setup_helm/remote_state.tm.hcl
-
-data "terraform_remote_state" "k8s_init" {
-  backend = "local"
-  config = {
-    path = "../../../../tfstate/stacks/bare/k8s/init/terraform.tfstate"
-  }
-}
